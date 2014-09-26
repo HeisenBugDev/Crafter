8c10a467295f5ad74de33c104fb3595e57d960fb
hunterboerner <hunterboerner@gmail.com>
2014-09-25 19:37:10 -0500

    override things

--- lib/crafter/cli_overrides.rb
+++ lib/crafter/cli_overrides.rb
@@ -1,33 +1,29 @@
-puts "LOAD"
 module CLIOverrides
-  def say(message, color: [], arrow_color: nil, bold: true, force_new_line: nil)
-    # collapse_message!(message)
-    puts "wat"
-    [message].to_cli
+  def low_say(message, color: [], arrow_color: nil, bold: true, force_new_line: nil)
+    collapse_message!(message)
     args = [force_new_line] if force_new_line
-    super(message, *([color].flatten + ([:bold] if bold)), *args)
+    colors = ([color] << (:bold if bold)).flatten.compact
+    say(message, colors, *args, superify: true)
+  end
+
+  def say(*args, superify: false)
+    return super *args if superify
+    arrow; low_say(*args)
   end
 
   def ask(message, *args)
-    collapse_message!(message)
+    collapse_message!(message); arrow
     message << "\n    >"
-    say('==> ', bold: false)
     super(message, )
   end
 
 private
 
-  def [](*args)
-    CustomArray.new(message)
+  def arrow
+    low_say('==> ', bold: false)
   end
 
   def collapse_message!(message)
     message = [message].flatten.join("\n    ")
-  end
-
-  class CustomArray << Array
-    def to_cli
-      [self].flatten.join("\n    ")
-    end
   end
 end
