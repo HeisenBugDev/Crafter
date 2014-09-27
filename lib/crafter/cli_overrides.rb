7f63d2d559258010ac3a888e1b5d9984a39603eb
hunterboerner <hunterboerner@gmail.com>
2014-09-27 12:24:31 -0500

    let's move this over real quick

--- lib/crafter/cli_overrides.rb
+++ lib/crafter/cli_overrides.rb
@@ -1,8 +1,9 @@
 module CLIOverrides
-  def low_say(message, color: [], arrow_color: nil, bold: true, force_new_line: nil)
+  def low_say(message,
+  colors: [], arrow_color: nil, bold: true, force_new_line: nil)
     collapse_message!(message)
     args = [force_new_line] if force_new_line
-    colors = ([color] << (:bold if bold)).flatten.compact
+    colors = ([colors] << (:bold if bold)).flatten.compact
     say(message, colors, *args, superify: true)
   end
 
@@ -18,6 +19,14 @@ module CLIOverrides
   end
 
 private
+
+  def determine_colors!(colors, *args)
+    colors = determine_colors(colors, *args)
+  end
+
+  def determine_colors(colors, *args)
+    ([colors] << (:bold if bold)).flatten.compact
+  end
 
   def arrow
     low_say('==> ', bold: false)
