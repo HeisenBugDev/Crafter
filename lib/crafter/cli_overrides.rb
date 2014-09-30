3a87f3b75f16f60e487bc17a48eef2941e1af9af
hunterboerner <hunterboerner@gmail.com>
2014-09-30 17:20:35 -0500

    More things

--- lib/crafter/cli_overrides.rb
+++ lib/crafter/cli_overrides.rb
@@ -4,9 +4,9 @@ module Crafter
   module CLIOverrides
     def low_say(message,
     colors: [], arrow_color: nil, bold: true, force_new_line: nil)
-      collapse_message!(message)
-      args = [force_new_line] if force_new_line
-      colors = ([colors] << (:bold if bold)).flatten.compact
+      stringify_message!(message)
+      conditional_arg(args = [], force_new_line)
+      colors = ([colors] << (:bold if bold)).compact
       say(message, colors, *args, superify: true)
     end
 
@@ -16,26 +16,23 @@ module Crafter
     end
 
     def ask(message, *args)
-      collapse_message!(message); arrow
+      stringify_message!(message); arrow
       message << "\n    >"
       super(message, )
     end
 
   private
 
-    def determine_colors!(colors, *args)
-      colors = determine_colors(colors, *args)
-    end
-
-    def determine_colors(colors, *args)
-      ([colors] << (:bold if bold)).flatten.compact
+    # Adds argument to array if it is truthy
+    def conditional_arg!(args_array, arg)
+      args_array << arg if arg
     end
 
     def arrow
       low_say('==> ', bold: false)
     end
 
-    def collapse_message!(message)
+    def stringify_message!(message)
       message = [message].flatten.join("\n    ")
     end
   end
