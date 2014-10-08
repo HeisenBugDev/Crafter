d95ba049db1fef02093dcf668bc1a833762d8124
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-10-08 00:28:17 -0600

    Create the folder structure of the crafter folder.

--- lib/crafter/filesystem.rb
+++ lib/crafter/filesystem.rb
@@ -1,7 +1,10 @@
 module Crafter
   class FileSystem
-    def self.create_crafter_directory
-      Dir.mkdir "#{ENV['HOME']}/.crafter/" unless Dir.exist? "#{ENV['HOME']}/.crafter"
+    def self.create_folder_structure
+      crafter_dir = "#{ENV['HOME']}/.crafter/"
+      Dir.mkdir crafter_dir unless Dir.exist? crafter_dir
+      Dir.mkdir "#{crafter_dir}/versions" unless Dir.exist? "#{crafter_dir}/versions"
+      Dir.mkdir "#{crafter_dir}/instances" unless Dir.exist? "#{crafter_dir}/instances"
     end
   end
 end
