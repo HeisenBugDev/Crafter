6df312dc4e7356a6431b2e227938966270eae7d9
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-10-08 00:28:57 -0600

    Allow defining of the root Crafter folder.

--- lib/crafter/filesystem.rb
+++ lib/crafter/filesystem.rb
@@ -1,7 +1,6 @@
 module Crafter
   class FileSystem
-    def self.create_folder_structure
-      crafter_dir = "#{ENV['HOME']}/.crafter/"
+    def self.create_folder_structure(crafter_dir = "#{ENV['HOME']}/.crafter/")
       Dir.mkdir crafter_dir unless Dir.exist? crafter_dir
       Dir.mkdir "#{crafter_dir}/versions" unless Dir.exist? "#{crafter_dir}/versions"
       Dir.mkdir "#{crafter_dir}/instances" unless Dir.exist? "#{crafter_dir}/instances"
