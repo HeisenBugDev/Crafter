4ffa639f7e21a4b0a146230027f0c443ae2c3e5f
hunterboerner <hunterboerner@gmail.com>
2014-10-11 14:05:02 -0500

    Things!

---
+++ lib/crafter/instance.rb
@@ -0,0 +1,19 @@
+module Crafter
+  class Instance
+    def initialize(instance_name)
+    end
+
+    attr_accessor :craftfile
+
+    # attr_accessor :minecraft
+    # attr_accessor :forge
+  end
+
+  class InstanceCache
+    class << self
+      def burst
+
+      end
+    end
+  end
+end
