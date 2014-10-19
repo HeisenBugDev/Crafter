c98aa535dd19d1e6cbb47a1bd6366eb9f0f8eb1c
hunterboerner <hunterboerner@gmail.com>
2014-10-19 14:27:45 -0500

    A fair bit of work on the instance management stuff

--- lib/crafter/filesystem.rb
+++ lib/crafter/filesystem.rb
@@ -1,32 +1,49 @@
 module Crafter
   class FileSystem
-    CRAFTER_DIR = "#{ENV['HOME']}/.crafter"
+    CRAFTER_DIR = File.join(Dir.home, '.crafter')
 
-    def self.base_directory
-      Dir.mkdir CRAFTER_DIR unless Dir.exist? CRAFTER_DIR
-      Dir.open CRAFTER_DIR
+    class << self
+      def base_directory
+        Dir.mkdir CRAFTER_DIR unless Dir.exist? CRAFTER_DIR
+        Dir.open CRAFTER_DIR
+      end
+
+      def versions_directory
+        dir = crafter_folder('versions')
+        Dir.mkdir dir unless Dir.exist? dir
+        Dir.open dir
+      end
+
+      def instances_directory
+        dir = crafter_folder('instances')
+        Dir.mkdir dir unless Dir.exist? dir
+        Dir.open dir
+      end
+
+      def instance_cache_location
+        File.join(instances_directory, "InstanceCache")
+      end
+
+      def base_directory_structure
+        [base_directory, versions_directory, instances_directory]
+      end
+
+      def fs_instance_list
+        Dir.glob("#{instances_directory.to_path}/*").
+          select { |f| File.directory? f }.
+          map { |path| File.basename path }
+      end
+
+      def instance_directory(name)
+        dir = crafter_folder('instances', name)
+        Dir.mkdir(dir) unless Dir.exist?(dir)
+        {dir: Dir.open(dir), craftfile: File.join(dir, 'Craftfile')}
+      end
+
+      def crafter_folder(*args)
+        File.join(CRAFTER_DIR, *args)
+      end
     end
 
-    def self.versions_directory
-      dir = "#{CRAFTER_DIR}/versions"
-      Dir.mkdir dir unless Dir.exist? dir
-      Dir.open dir
-    end
-
-    def self.instances_directory
-      dir = "#{CRAFTER_DIR}/instances"
-      Dir.mkdir dir unless Dir.exist? dir
-      Dir.open dir
-    end
-
-    def self.base_directory_structure
-      [base_directory, versions_directory, instances_directory]
-    end
-
-    def self.instance_directory(name, options = {})
-      dir = "#{CRAFTER_DIR}/instances/#{name}"
-      Dir.mkdir dir unless Dir.exist? dir
-      Dir.open dir
-    end
   end
 end
--- lib/crafter/instance.rb
+++ lib/crafter/instance.rb
@@ -1,19 +1,64 @@
 module Crafter
-  class Instance
-    def initialize(instance_name)
-    end
+  @@instance_cache = nil
 
+  def self.instance_cache
+    @@instance_cache
+  end
+
+  def self.instance_cache=(instance_cache)
+    @@instance_cache = instance_cache
+  end
+
+  class Instance
     attr_accessor :craftfile
 
-    # attr_accessor :minecraft
-    # attr_accessor :forge
+    def initialize(name)
+      @craftfile = Crafter::FileSystem.instance_directory(name)[:craftfile]
+    end
+
+    def delete
+      InstanceCache.burst(self)
+    end
   end
 
   class InstanceCache
-    class << self
-      def burst
+    def initialize()
+      @instances = [] # It's super effective!
+    end
 
+    def load
+      Crafter.instance_cache =
+        Marshal.load(File.read(Crafter::FileSystem.instance_cache_location))
+    end
+
+    def save
+      File.open(Crafter::FileSystem.instance_cache_location, "w")
+        { |f| f.write(Marshal.dump(self)) }
+    end
+
+    def burst(instance = nil)
+      if instance
+        @instances.delete(instance)
+        recalculate_instance_cache(instance.name)
+      else
+        @instances = []
+        recalclate_all_instance_caches
       end
+    end
+
+    def find(name)
+
+    end
+
+    private
+
+    def recalculate_instance_cache(instance_name)
+      # Recalculate it
+    end
+
+    def recalculate_all_instance_caches
+      Crafter::FileSystem.fs_instance_list.
+        each { |e| recalculate_instance_cache(e) }
     end
   end
 end
